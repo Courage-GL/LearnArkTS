113b684e53652cae8f9bae6743145b929f302981
gaolei <1450030827@qq.com>
2024-04-02 08:44:15 +0800

    1.登录界面 2.我的设置界面

---
+++ HWProject/MyMallApp/entry/src/main/ets/pages/CommonConstants.js
@@ -0,0 +1,15 @@
+/**
+ * 项目使用到的常量
+ *
+ * */
+export default class CommonConstants {
+}
+CommonConstants.INPUT_ACCOUNT_LENGTH = 11;
+CommonConstants.INPUT_PWD_LENGTH = 8;
+CommonConstants.INPUT_PADDING_LEFT = 0;
+CommonConstants.LOGIN_DELAY_TIME = 2000;
+CommonConstants.COMMON_SPACE = 12;
+CommonConstants.HOME_TITLE = '首页';
+CommonConstants.MINE_TITLE = '我的';
+CommonConstants.FULL_PARENT = '100%';
+//# sourceMappingURL=CommonConstants.js.map
